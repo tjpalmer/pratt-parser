7ee27fc63ce40a81ec3afe4cff8437d822e29264
Andy Grove <andygrove73@gmail.com>
2016-08-13 08:47:50 -0600

    implemented prefix parser

--- src/parser.rs
+++ src/parser.rs
@@ -19,14 +19,22 @@ enum Op {
 }
 
 fn parse(tokens: Vec<Token>) -> Result<Expr, &'static str> {
-    let it = tokens.iter().peekable();
-    // parse_prefix(&it);
+    let mut it = tokens.iter().peekable();
+    parse_prefix(&mut it);
     Err("not implemented yet")
 }
 
-// fn parse_prefix(it: &Peekable<Token>) -> Result<Expr, &'static str> {
-//     Err("not implemented yet")
-// }
+fn parse_prefix<'a, It>(it: &mut Peekable<It>) -> Result<Expr, &'static str>
+    where It: Iterator<Item=&'a Token> {
+
+    match it.peek() {
+        Some(&t) => match t {
+            &Token::Integer(n) => Ok(Expr::Integer(n)),
+            _ => Err("TBD")
+        },
+        None => Err("no more tokens")
+    }
+}
 //
 // fn parse_infix() -> Result<Expr, &'static str> {
 //     Err("not implemented yet")
